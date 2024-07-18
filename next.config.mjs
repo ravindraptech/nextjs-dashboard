db472c2a60654036e42af8c8003d6b4542e10f69
ravindraptech <ravindrap.tech@gmail.com>
2024-07-18 09:59:19 +0530

    comment out ppr config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
+    /*experimental: {
         ppr: 'incremental',
-  },
+  },*/
 };
 
 export default nextConfig;
